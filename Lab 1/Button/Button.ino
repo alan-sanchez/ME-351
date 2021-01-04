400d41834772a4a152f4d5edf0b83c7459e40080
Alan G. Sanchez <sanchala@oregonstate.edu>
2021-01-03 21:24:50 -0800

    Include control of external LED with button code

--- Lab 1/Button/Button.ino
+++ Lab 1/Button/Button.ino
@@ -5,12 +5,13 @@
 
 // constants won't change. They're used here to set pin numbers:
 const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
+const int ledPin =  7;      // the number of the LED pin
 
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
 
 void setup() {
+  Serial.begin(9600);
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
   // initialize the pushbutton pin as an input:
@@ -20,13 +21,14 @@ void setup() {
 void loop() {
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
+  if (buttonState == LOW) {
     // turn LED on:
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin, LOW);
   } else {
     // turn LED off:
-    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin, HIGH);
   }
 }
