33b8e6bddc9d9f2329b7fde369e8d4e192bdf02e
Alan G. Sanchez <sanchala@oregonstate.edu>
2021-01-07 22:32:05 -0800

    Init commit of Part 3

---
+++ Lab 2/Part_3/Part_3.ino
@@ -0,0 +1,41 @@
+/*
+ Lab 2 part 2 Sign Off
+*/
+
+// digital pin 2 has a photoresistor attached to it.
+int photo = 2;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  
+  // make the potentiometer's pin an input:
+  pinMode(pot, INPUT);
+
+  // print top line
+  Serial.println(" Time (ms), Input Value (V), Input Value (digital)");
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0)*.0049;
+  
+  // read the input on digital pin 2:
+  int buttonState = digitalRead(pot);
+
+  // read the current time
+  int time = millis(); 
+  
+  // print out the value you read:
+  Serial.print(time);
+  Serial.print(",");
+  Serial.print(sensorValue);
+  Serial.print(",");
+  Serial.println(buttonState);
+  
+  delay(100);        // delay in between reads for stability
+}
