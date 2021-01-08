4dad3d2704d6086bd62c8fad130dab231098ad61
Alan G. Sanchez <sanchala@oregonstate.edu>
2021-01-08 11:05:49 -0800

    Compute thermistor resistance and temperature

--- Lab 2/Part_4/Part_4.ino
+++ Lab 2/Part_4/Part_4.ino
@@ -23,17 +23,21 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   
-  // read the input on analog pin 0:
+  // read the input on analog pin 0 and convert to volts:
   float Vo = analogRead(A0)*.0049;
 
   // Compute Resistance of Thermo
   float R1 = (Vi / Vo * R2) - R2;
+
+  // Convert Resistance to temp but using Steinhart-Hart equation.
+  float tempC = (1 / (0.001129148 + 0.000234125 * log(R1) + 0.0000000876741 * pow(log(R1), 3))) - 273.15;
+
   // read the current time
   int time = millis(); 
   
   // print out the value you read:
   Serial.print(time);
   Serial.print(",");
-  Serial.println(R1);  
-  delay(10);        // delay in between reads for stability
+  Serial.println(tempC);  
+  delay(100);        // delay in between reads for stability
 }
