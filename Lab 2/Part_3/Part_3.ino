2479d0b92d6bf90547144bfe1e280b83b71aa078
Alan G. Sanchez <sanchala@oregonstate.edu>
2021-01-11 21:55:31 -0800

    Change volt value from int to volt

--- Lab 2/Part_3/Part_3.ino
+++ Lab 2/Part_3/Part_3.ino
@@ -1,9 +1,10 @@
 /*
- Lab 2 part 2 Sign Off
+ Lab 2 part 3 Sign Off
 */
 
 // digital pin 2 has a photoresistor attached to it.
 int photo = 2;
+int eps = 0.0;
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -22,14 +23,14 @@ void setup() {
 void loop() {
   
   // read the input on analog pin 0:
-  float sensorValue = analogRead(A0)*.0049;
-  
+  int sensorValue = analogRead(A0);
+  float eps = sensorValue * 5.0 / 1023.0;
   // read the current time
   int time = millis(); 
   
   // print out the value you read:
   Serial.print(time);
   Serial.print(",");
-  Serial.println(sensorValue);  
+  Serial.println(eps);  
   delay(10);        // delay in between reads for stability
 }
