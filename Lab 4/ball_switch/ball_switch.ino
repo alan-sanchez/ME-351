d595e94f2fba29517960880631f33e7fa0b4002b
Alan G. Sanchez <sanchala@oregonstate.edu>
2021-01-31 15:52:38 -0800

    Modified if else statement to count only when state of the sensor changed from low to high

--- Lab 4/ball_switch/ball_switch.ino
+++ Lab 4/ball_switch/ball_switch.ino
@@ -4,21 +4,32 @@
 const byte LED_PIN = LED_BUILTIN; // LED_BUILTIN = 13 on the Uno
 const byte SENSOR_PIN = 2;
 int state = 0;
+volatile int count = 0;
+int prev_state = 0;
 
 void setup() {
   // Set up pins
   pinMode(LED_PIN, OUTPUT);//initialize the ledPin as an output
   pinMode(SENSOR_PIN, INPUT_PULLUP);
+  Serial.begin(9600);
+
 }
 
 void loop() {
   state = digitalRead(SENSOR_PIN);
+  Serial.print(state);
+  Serial.print(", ");
+
   // Check the sensor state and act accordingly
-  if (state == HIGH) {
-    // Disconnected; turn the LED off
-    digitalWrite(LED_PIN, HIGH);
-  } else {
-    // Connected; turn the LED on
-    digitalWrite(LED_PIN, LOW);
+  if (state == HIGH && prev_state == LOW) {
+    count = count + 1;
+    prev_state = HIGH;
   }
+
+  else if (state == LOW) {
+    prev_state = LOW;
+  }
+  Serial.println(count);
+
+  delay(100);
 }
